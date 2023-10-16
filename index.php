872f0bd148ed05c03f3d02e021c0844b05575ce4
Grzegorz Piasny <106985215+GP233b@users.noreply.github.com>
2023-10-16 10:47:32 +0200

    html

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-
-echo 'Dzień dobry Polsko';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        echo "Dzień dobry, Polsko!";
+    ?>
+</body>
+</html>
